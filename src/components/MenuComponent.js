bc9102f1afc0b2a201f3330e232cc13512c01581
Medrouad <med.rouad@gmail.com>
2020-04-20 16:05:03 +0200

    First Assignment

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,5 @@
 import React,{ Component } from 'react';
-import {Card, CardImg, CardImgOverlay, CardText, CardTitle, CardBody} from 'reactstrap'
+import {Card, CardImg, CardImgOverlay, CardText, CardTitle, CardBody} from 'reactstrap';
 
 
 class Menu extends Component {
@@ -11,18 +11,39 @@ class Menu extends Component {
 }
 
         onDishSelect(dish) {
-            this.setState({selectedDish: dish})
+            this.setState({selectedDish: dish});
         }
+
         renderDish(dish) {
             if(dish != null) {
                 return(
-                    <Card>
-                        <CardImg top src={dish.image} alt={dish.name}/>
-                            <CardBody>
-                                <CardTitle>{dish.name}</CardTitle>
-                                <CardText>{dish.description}</CardText>
-                            </CardBody>
-                    </Card>
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            <Card>
+                                <CardImg top src={dish.image} alt={dish.name}/>
+                                    <CardBody>
+                                        <h2 className="card-title">{dish.name}</h2>
+                                        <CardText>{dish.description}</CardText>
+                                    </CardBody>
+                            </Card>
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                            <Card>
+                                <CardBody>
+                                    <h2 className="card-title">Comments</h2>
+                                    <CardText>{dish.comments.map( com => {
+                                        return (
+                                            <div>  
+                                                <CardText>{com.comment}</CardText>
+                                                <CardText>--{com.author}, {com.date}</CardText>
+                                            </div>  
+                                        )
+                                    })}
+                                    </CardText>
+                                </CardBody>
+                            </Card>
+                     </div>
+                    </div>
                 )
             }
             else {
@@ -49,12 +70,11 @@ class Menu extends Component {
             return (
                 <div className="container">
                     <div className="row">
-                    {menu}
+                         {menu}
                     </div>
-                    <div className="row">
-                        {this.renderDish(this.state.selectedDish)}
+                    <div>
+                        {this.renderDish(this.state.selectedDish)}            
                     </div>
-            
                 </div>
             )
     }
